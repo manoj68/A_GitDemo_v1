fe1b34fbc41e70f6841ae961076040013b7cb8ef
Manoj.Sutar <Manoj.Sutar@MhaLap046.mshome.net>
2021-04-26 18:12:42 +0530

    local branch created - test

---
+++ src/test/java/testOne/Testclass4.java
@@ -0,0 +1,19 @@
+package testOne;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+
+public class Testclass4 
+{
+	@Test
+	public void demo()
+	{
+	ArrayList ar = new ArrayList();
+	ar.add(10);
+	ar.add("20");
+	
+	System.out.println("Hellooooo");
+	}
+
+}
